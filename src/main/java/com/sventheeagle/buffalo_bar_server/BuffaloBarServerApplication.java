0eb66e61dfe77ff6d2f5bf8e92b2ede4d2e67993
Sven <sven@home.lu>
2024-08-05 22:09:41 +0200

    add GET hello

--- src/main/java/com/sventheeagle/buffalo_bar_server/BuffaloBarServerApplication.java
+++ src/main/java/com/sventheeagle/buffalo_bar_server/BuffaloBarServerApplication.java
@@ -2,12 +2,19 @@ package com.sventheeagle.buffalo_bar_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BuffaloBarServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuffaloBarServerApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String greet() {
+		return "Hello";
+	}
 }
