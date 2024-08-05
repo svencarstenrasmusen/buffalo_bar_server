bfaaf2bffb13b1932e70b792324443eab8c3f0fb
Sven <sven@home.lu>
2024-08-05 21:52:14 +0200

    first commit

---
+++ src/main/java/com/sventheeagle/buffalo_bar_server/BuffaloBarServerApplication.java
@@ -0,0 +1,13 @@
+package com.sventheeagle.buffalo_bar_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffaloBarServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffaloBarServerApplication.class, args);
+	}
+
+}
